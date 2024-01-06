3c4d438c4722a919c3dff25d0a8c05dc67a5758b
Adesanya Abdullateef Adebowale <debo@abdullateet.dev>
2024-01-06 21:45:07 +0100

    Add files via upload

---
+++ while loop.js
@@ -0,0 +1,36 @@
+/*
+var i = 1;
+while (i < 31) {
+    console.log(i);
+    i = i + 1;
+}
+*/
+
+/*
+var i = 1;
+
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+console.log('Counting completed!');
+
+*/
+
+/*
+var i = 5;
+while (i > 0) {
+    console.log(i);
+    i = i - 1;
+
+}
+console.log("mad")
+*/
+
+
+var i =2022;
+while (i >= 2018) {
+    console.log(i)
+    i = i - 1;
+}
